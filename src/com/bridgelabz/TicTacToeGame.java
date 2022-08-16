b6a98ac1100c94dbd20a2e4600c3f1d008a38216
Surabhi951 <surabhibhagat95@gmail.com>
2022-08-16 19:15:38 +0530

    Added: choose letter x or o

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -1,15 +1,27 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
     static char[] board = new char[10];
+    static char userLetter;
+    static char computerLetter;
 
     public static void main(String[] args) {
         createEmptyBoard();
+        chooseLetter();
     }
 
     static void createEmptyBoard(){
         for(int index = 1; index < board.length; index++){
             board[index] = ' ';
         }
+    }
+
+    static void chooseLetter(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("choose a letter :: X or O :");
+        userLetter = sc.next().toUpperCase().charAt(0);
+        computerLetter = (userLetter == 'X') ? 'O' : 'X';
     }
 }
